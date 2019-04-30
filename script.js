228209d2e395b11ba0ab5ad5f027f32a0db1f70a
Sahil Maharjan <sahil.maharjan678@gmail.com>
2019-04-30 13:31:35 +0545

    initial commit

---
+++ script.js
@@ -0,0 +1,150 @@
+var cells = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+$(document).ready(function () {
+
+    var table = $('table');
+    var player = 1;
+    var pattern;
+
+    var message = $('.message');
+
+
+    message.text(`X's turn`);
+
+    // $('td').on('mouseenter', function () {
+    //     if (player === 1) {
+    //         $(this).addClass('crosshover')
+    //     } else if (player === 2) {
+    //         $(this).addClass('criclehover')
+    //     }
+    // }).on('mouseleave', function () {
+    //     $(this).removeClass('circlehover')
+    //     console.log($(this))
+    // })
+
+
+
+    $('td').one("click", function () {
+        // console.log(cells)
+
+        td = $(this);
+        var id = td.data().id;
+        // console.log(id)
+        if (player == 1) {
+            message.text(`O's turn`);
+            pattern = 'cross';
+            td.addClass(pattern);
+            cells.splice(id, 1, 'X');
+            // td.removeClass('crosshover');
+            // td.removeClass('circlehover');
+            // td.off('hover');
+            // $(this).mouseover(function () {
+            //     console.log("hovered", id)
+            // })
+            // console.log((td.hasClass('circle') || td.hasClass('cross')))
+            player = 2;
+            win = hasPlayerWon();
+            if (win == 1) {
+                message.text(`X has Won`).css({
+                    'color': 'red'
+                });
+                setTimeout(function () {
+                    reset();
+                }, 3000)
+            }
+        } else if (player == 2) {
+            message.text(`X's turn`);
+            pattern = 'circle'
+            td.addClass(pattern);
+            cells.splice(id, 1, 'O')
+            // td.removeClass('circlehover');
+            // td.removeClass('crosshover');
+            player = 1;
+            win = hasPlayerWon(table, pattern);
+
+            if (win == 1) {
+                message.text(`O has Won`).css({
+                    'color': 'blue'
+                });
+                setTimeout(function () {
+                    reset();
+                }, 3000)
+            }
+        }
+        if (win == 'draw') {
+            message.text(`It is a Draw`).css({
+                'color': 'green'
+            });
+            setTimeout(function () {
+                reset();
+            }, 3000)
+        }
+        // td.unbind('mouseenter mouseleave');
+    });
+
+
+
+
+    $('.reset').click(function () {
+        location.reload(true);
+    })
+});
+
+function reset() {
+    location.reload(true);
+}
+
+function hasPlayerWon(table, pattern) {
+    win = 0;
+    var winningCondition = [
+        [1, 2, 3],
+        [4, 5, 6],
+        [7, 8, 9],
+        [1, 4, 7],
+        [2, 5, 8],
+        [3, 6, 9],
+        [1, 5, 9],
+        [3, 5, 7]
+    ]
+    for (var index = 0; index < winningCondition.length; index++) {
+        var winCond = winningCondition[index];
+        if (cells[winCond[0]] !== 0 && cells[winCond[0]] === cells[winCond[1]] && cells[winCond[1]] === cells[winCond[2]]) {
+            return 1;
+        }
+
+    }
+    var allCellsFilled = 1;
+    for (var index = 1; index < cells.length; index++) {
+        if (!cells[index]) {
+            allCellsFilled = 0;
+            break;
+        }
+    }
+    if (allCellsFilled) {
+        return 'draw';
+    }
+    // if (table.find('#1').hasClass(pattern) && table.find('#2').hasClass(pattern) && table.find('#3').hasClass(pattern)) {
+    //     win = 1;
+    // } else if (table.find('#4').hasClass(pattern) && table.find('#5').hasClass(pattern) && table.find('#6').hasClass(pattern)) {
+    //     win = 1;
+    // } else if (table.find('#7').hasClass(pattern) && table.find('#8').hasClass(pattern) && table.find('#9').hasClass(pattern)) {
+    //     win = 1;
+    // } else if (table.find('#1').hasClass(pattern) && table.find('#4').hasClass(pattern) && table.find('#7').hasClass(pattern)) {
+    //     win = 1;
+    // } else if (table.find('#2').hasClass(pattern) && table.find('#5').hasClass(pattern) && table.find('#8').hasClass(pattern)) {
+    //     win = 1;
+    // } else if (table.find('#3').hasClass(pattern) && table.find('#6').hasClass(pattern) && table.find('#9').hasClass(pattern)) {
+    //     win = 1;
+    // } else if (table.find('#1').hasClass(pattern) && table.find('#5').hasClass(pattern) && table.find('#9').hasClass(pattern)) {
+    //     win = 1;
+    // } else if (table.find('#3').hasClass(pattern) && table.find('#5').hasClass(pattern) && table.find('#7').hasClass(pattern)) {
+    //     win = 1;
+    // } else if ((table.find('#1').hasClass('cross') || table.find('#1').hasClass('circle')) && (table.find('#2').hasClass('cross') || table.find('#2').hasClass('circle')) &&
+    //     (table.find('#3').hasClass('cross') || table.find('#3').hasClass('circle')) && (table.find('#4').hasClass('cross') || table.find('#4').hasClass('circle')) &&
+    //     (table.find('#5').hasClass('cross') || table.find('#5').hasClass('circle')) && (table.find('#6').hasClass('cross') || table.find('#6').hasClass('circle')) &&
+    //     (table.find('#7').hasClass('cross') || table.find('#7').hasClass('circle')) && (table.find('#8').hasClass('cross') || table.find('#8').hasClass('circle')) &&
+    //     (table.find('#9').hasClass('cross') || table.find('#9').hasClass('circle'))) {
+    //     win = "draw";
+    // }
+
+    // return win;
+}
